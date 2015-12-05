b0d4117bc3dcf731116ead608ead6c828306f61a
Daniel Hallqvist <dhallqvist91@gmail.com>
2015-12-05 23:58:47 +0100

    cleanup 5

--- solutions/5.js
+++ solutions/5.js
@@ -6,11 +6,11 @@ let file = fs.readFile(fileName, 'utf-8', (err, content) => {
   let splittedLines = content.split(/\r\n/);
   let lines = {};
   let result = splittedLines.reduce((count, current) => {
-    let wordArray = current.split('').sort().join();
-    lines[wordArray] = (lines[wordArray] || 0) + 1;
+    let sortedWord = current.split('').sort().join();
+    lines[sortedWord] = (lines[sortedWord] || 0) + 1;
 
-    return lines[wordArray] > 1 ?
-           (lines[wordArray] == 2 ? count + 2 : count + 1)
+    return lines[sortedWord] > 1 ?
+           (lines[sortedWord] == 2 ? count + 2 : count + 1)
            : count;
   }, 0);
 
