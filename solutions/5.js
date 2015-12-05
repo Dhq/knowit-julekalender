d67da4941ab04e2c6b5d7116b96cae439363e6ce
Daniel Hallqvist <dhallqvist91@gmail.com>
2015-12-05 23:53:53 +0100

    prettified nr 2

--- solutions/5.js
+++ solutions/5.js
@@ -4,20 +4,16 @@ console.time('ran');
 let fileName = './inputs/5.txt';
 let file = fs.readFile(fileName, 'utf-8', (err, content) => {
   let splittedLines = content.split(/\r\n/);
-  let lines = [];
-  let result = 0;
-  splittedLines.forEach((current) => {
+  let lines = {};
+  let result = splittedLines.reduce((count, current) => {
     let wordArray = current.split('').sort().join();
-    if(wordArray.length > 0) lines.push(wordArray);
-  });
+    lines[wordArray] = (lines[wordArray] || 0) + 1;
 
-  let counts = {};
-  lines.forEach((x) => { counts[x] = (counts[x] || 0) + 1; });
-  for (var c in counts) {
-    if (counts.hasOwnProperty(c) && counts[c] > 1) {
-      result += counts[c];
-    }
-  }
+    return lines[wordArray] > 1 ?
+           (lines[wordArray] == 2 ? count + 2 : count + 1)
+           : count;
+  }, 0);
+
   console.log(`Anagrams found: ${result}`);
   console.timeEnd('ran');
 });
