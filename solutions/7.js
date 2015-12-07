b76f68949f1e284b10dcd956f6c7e55494ad0be0
Daniel Hallqvist <dhallqvist91@gmail.com>
2015-12-07 16:02:46 +0100

    Fixes 7

--- solutions/7.js
+++ solutions/7.js
@@ -10,7 +10,7 @@ let result = 0;
 for (var i = 0; i <= 1000; i++) {
   if(i % 7 == 0) {
     let reversed = parseInt(i.toString().split('').reverse().join().replace(/,/g, ''));
-    if(reversed > 0 && 0 <= 1000 && reversed % 7 == 0) {
+    if(reversed % 7 == 0) {
         result += i;
     }
   }
