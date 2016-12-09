2f7b980074cb002da082598e8ad45c0988345ab1
Daniel Hallqvist <Ekstern_Daniel.Hallqvist@spv.no>
2016-12-09 13:17:29 +0100

    2016-9

---
+++ solutions/2016-9.js
@@ -0,0 +1,28 @@
+import fs from 'fs';
+
+console.time('1');
+let fileName = './inputs/2016-9.txt';
+let file = fs.readFile(fileName, 'utf-8', (err, content) => {
+    const transactions = content.split(/\n/);
+
+    let data = transactions.reduce((accounts, current) => {
+        let transaction = current.split(',');
+        let amount = parseInt(transaction[2]);
+
+        if(accounts[transaction[0]] !== "None") {
+            accounts[transaction[0]] = typeof(accounts[transaction[0]]) === "undefined" ? 0 : accounts[transaction[0]];
+            accounts[transaction[0]] -= amount;
+        }
+
+        accounts[transaction[1]] = typeof(accounts[transaction[1]]) === "undefined" ? 0 : accounts[transaction[1]];
+        accounts[transaction[1]] += amount;
+ 
+        return accounts;
+    }, []);
+
+    let res = Object.keys(data).filter((id) => data[id] > 10).length;
+    console.log(res);
+
+    console.timeEnd('1');
+})
+
